dea953a7a825ec0746803d43c4c390892382b67c
hajinoori511 <noorizaykahn889@gmail.com>
2025-01-21 11:32:42 +0430

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "lS12DQrD#nKUQzC86KRFXV0UNVCRpGW3P6y4QVyANRutCikCTt-I",
     SUDO_NB: process.env.SUDO_NB || "923237045919",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
